c8faef90ff90a75fc1a42edec15488a04f884e7c
Aryan Bhalerao <aryan.bhalerao@protonmail.com>
2024-11-23 17:30:02 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+# Title
+st.title("Text Summarization using BERT2BERT Encoder Decoder Model")
+
+# Subheader
+st.subheader("Output:")
+
+# Text
+st.write("Model Trained Successfully!\nAccuracy:\nTraining Loss - Precision - Recall - F-Measure\n7.481323\t7.709305\t0.004800\t0.013600\t0.007000")
