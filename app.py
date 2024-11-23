6c787b6646c2112c6e20d160d58872a894c39f8d
Aryan Bhalerao <aryan.bhalerao@protonmail.com>
2024-11-23 22:15:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,10 @@ import streamlit as st
 st.title("Text Summarization using BERT2BERT Encoder Decoder Model")
 
 # Subheader
-st.subheader("Output:")
+st.subheader("Model Info:")
 
 # Text
-st.write("Model Trained Successfully!")
-
-# Text
-st.write("Accuracy:\nTraining Loss - Precision - Recall - F-Measure")
+st.write("Model Trained Successfully with following Rouge Scores: ")
 
 # Text
 st.write("Training Loss - Precision - Recall - F-Measure")
@@ -20,3 +17,11 @@ st.write("7.481323 - 7.709305 - 0.004800 - 0.013600 - 0.007000")
 
 # Display Image
 st.image("comparison.png", caption="Comparison with Other Models", use_column_width=True)
+
+# Subheader
+st.subheader("Example:")
+
+if st.button("Run"):
+    st.success("Summary")
+else:
+    st.info("Text")
